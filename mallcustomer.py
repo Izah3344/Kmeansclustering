f208ad6096b8394e747728312ea891762ca6c44c
Hafizah Rahim <112919251+Izah3344@users.noreply.github.com>
2022-09-15 20:09:03 +0800

    Update mallcustomer.py

--- mallcustomer.py
+++ mallcustomer.py
@@ -1,8 +1,4 @@
-#import pandas as pd 
-#import numpy as np 
-#import matplotlib.pyplot as plt 
-#import seaborn as sb 
-#from sklearn.cluster import KMeans
+#Import Packages
 
 import streamlit as st
 import matplotlib.pyplot as plt
@@ -13,14 +9,32 @@ import plotly.tools
 file = "mall_customer.csv"
 df = pd.read_csv(file)
 
-features = ['Annual_Income_(k$)', 'Spending_Score']
-X = df[features]
-plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'],  );
 
-kmeans = KMeans(n_clusters=5)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
-plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=25, cmap='viridis')
+st.header("My Mall Customer App")
 
-centers = kmeans.cluster_centers_
-plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
+option = st.sidebar.selectbox(
+    'Select a mini project',
+     ['whole data', 'description','KMeans cluster'])
+
+if option=='whole data':
+    whole = df
+    st.table(whole)
+    
+if option=='description':
+    chart_data = df.describe()
+    st.table(chart_data)
+
+else:
+    #Preprocessing
+    features = ['Annual_Income_(k$)', 'Spending_Score']
+    X = df[features]
+    plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score']);
+    kmeans = KMeans(n_clusters=5)
+    kmeans.fit(X)
+    y_kmeans = kmeans.predict(X)
+    plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
+    centers = kmeans.cluster_centers_
+    plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5);
+    st.pyplot()
+    
+    st.set_option('deprecation.showPyplotGlobalUse', False)
